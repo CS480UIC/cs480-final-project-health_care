4e80d6cc59eff39810e59cd98eec5249443d8849
Sanket Patel <59983795+spate541@users.noreply.github.com>
2021-11-13 16:15:03 -0600

    Update deleteDB.sql
    SQL script updated to delete the tables with their content.

--- sql/deleteDB.sql
+++ sql/deleteDB.sql
@@ -1,1 +1,13 @@
+-- SQL script to delete all the table of the database health_care
 
+DROP TABLE IF EXISTS bill;
+DROP TABLE IF EXISTS department;
+DROP TABLE IF EXISTS doctor;
+DROP TABLE IF EXISTS entity1;
+DROP TABLE IF EXISTS insurance;
+DROP TABLE IF EXISTS patient;
+DROP TABLE IF EXISTS patientdoctor;
+DROP TABLE IF EXISTS payment;
+DROP TABLE IF EXISTS prescription;
+DROP TABLE IF EXISTS room;
+DROP TABLE IF EXISTS user;
